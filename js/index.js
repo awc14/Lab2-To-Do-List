e22652bf7909289721bc6e177c4bb559b50f24be
Waseh <wasehcheema@gmail.com>
2024-03-11 04:58:48 -0600

    Intial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import {getToDoData} from './models/toDoListModel'
+import {toDoListView} from './views/toDoListView'
+async function appInit() {
+    getToDoData()
+    toDoListView()
+}
+
+appInit()
