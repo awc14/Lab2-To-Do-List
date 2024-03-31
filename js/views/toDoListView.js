974cedd61392a4220e0d8e5f7c068b62bca48ca0
awc14 <133934865+awc14@users.noreply.github.com>
2024-03-31 17:19:13 -0600

    Update toDoListView.js

--- js/views/toDoListView.js
+++ js/views/toDoListView.js
@@ -1,38 +1,38 @@
-import {toDoItemTemplate} from "../templates/toDoItemTemplate"
-import {subscribe} from '../models/toDoListModel'
-import {deleteToDoController} from "../controllers/deleteToDoController";
+import { toDoItemTemplate } from "../templates/toDoItemTemplate";
+import { subscribe } from '../models/toDoListModel';
+import { deleteToDoController } from "../controllers/deleteToDoController";
 import { updateToDoController } from "../controllers/updateToDoController";
-let view;
+import { addToDoController } from "../controllers/addToDoController";  
 
 export function toDoListView() {
-    view = document.querySelector('#to-do-list')
-    view.addEventListener('click', onHandleClick)
+    const addButton = document.querySelector('#add');
+    addButton.addEventListener('click', addToDoController);  
 }
 
-subscribe(render)
+subscribe(render);
 
 function render(data) {
-    const div = document.createElement('div')
-    const toDoList = document.querySelector('#item-container')
-    toDoList.replaceChildren()
-   
-    data.forEach((item) => {
-        div.prepend(toDoItemTemplate(item))
-    })
+    const div = document.createElement('div');
+    const toDoList = document.querySelector('#item-container');
+    toDoList.replaceChildren();
 
-    toDoList.append(div)
+    data.forEach((item) => {
+        div.prepend(toDoItemTemplate(item));
+    });
+
+    toDoList.append(div);
 }
 
 function onHandleClick(e) {
     switch (e.target.id) {
-        case 'delete': 
-            deleteToDoController(e.target.dataset.uid)
-            break
+        case 'delete':
+            deleteToDoController(e.target.dataset.uid);
+            break;
         case 'edit':
-            console.log(e.target.dataset.uid)
-            updateToDoController(e.target.dataset.uid)
-            break
+            console.log(e.target.dataset.uid);
+            updateToDoController(e.target.dataset.uid);
+            break;
         default:
-            null
+            null;
     }
 }
